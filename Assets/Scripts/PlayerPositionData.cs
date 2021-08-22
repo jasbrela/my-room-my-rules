06f6768a84f4f6fe68579219efafde88c5f9b4da
jasbrela <gabriela@manifestohut.com.br>
2021-08-22 13:39:33 -0300

    add player position data

---
+++ Assets/Scripts/PlayerPositionData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PlayerPositionData : ScriptableObject
+{
+    public Transform bookPlayerPosition;
+    public Transform skeletonPlayerPosition;
+    public Transform actionFigurePlayerPosition;
+    public Transform notebookPlayerPosition;
+}
